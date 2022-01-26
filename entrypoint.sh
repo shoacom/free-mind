935a1439fc8e3324f97e2ca933f672cd79ec0f6f
shoacom <837907540@qq.com>
2022-01-26 19:41:32 +0800

    rollback

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/usr/local/etc/v2ray"
+DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
@@ -30,15 +30,17 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/download/v5.0.3/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
-chmod +x install-release.sh
-chmod +x install-dat-release.sh
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
-./install-release.sh
-./install-dat-release.sh
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
 
-cp ${DIR_TMP}/heroku.json ${DIR_CONFIG}/config.json
-
-/usr/local/bin/v2ray -config /usr/local/etc/v2ray/config.json
+# Run V2Ray
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
